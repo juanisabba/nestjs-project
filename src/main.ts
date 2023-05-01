f19806ce4990cc10d8d1c1b54d47ce0e56b12e95
JUANI\juani <juanisabbatini@gmail.com>
2023-05-01 17:36:19 +0200

    unnecesary commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('first');
   await app.listen(3000);
 }
 bootstrap();
